ad95433d05417d2fc96c7c56ab31b5a67b3f3713
YilmazDegerli <115919886+YilmazDegerli@users.noreply.github.com>
2022-11-03 10:44:25 +0100

    Update SQL_Ödev5.sql

--- SQL_Ödev5.sql
+++ SQL_Ödev5.sql
@@ -1,5 +1,5 @@
-1. 
+1. SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
 
-2.  
+2. SELECT * FROM film WHERE title LIKE '%n' ORDER BY length ASC OFFSET 5 LIMIT 5;
 
-3. 
+3. SELECT * FROM customer Where store_id = 1 ORDER BY last_name DESC LIMIT 4;
